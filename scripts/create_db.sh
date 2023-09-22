8f2b518d72738d62a8db8296b613efd0c60c36d9
Stanislas Bruhiere <stanislas@bruhiere.fr>
2023-09-22 11:26:46 +0200

    feat: add new env variables

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -4,30 +4,32 @@
 
 set -euo pipefail
 
-new_user=$PGUSER
-new_password=$PGPASSWORD
-new_database=$PGDATABASE
+hostname=$POSTGRES_HOST
 
-admin_user=$ADMINPGUSER
-admin_password=$ADMINPGPASSWORD
+new_user=$POSTGRES_USER
+new_password=$POSTGRES_PASSWORD
+new_database=$POSTGRES_DATABASE
 
-origin_database=$ORIGINPGDATABASE
+admin_user=$ADMIN_POSTGRES_USER
+admin_password=$ADMIN_POSTGRES_PASSWORD
 
-DUMP_FILE="/tmp/$origin_database.dump"
+origin_database=$ORIGIN_POSTGRES_DATABASE
+
+dump_file="/tmp/$origin_database.dump"
 
 # Create the database with admin creds
 echo "Creating database $new_database with user $new_user"
-PGPASSWORD="$new_password" PGHOST="$PGHOST" PGUSER="$new_user" createdb "$new_database"
+PGPASSWORD="$new_password" PGHOST="$hostname" PGUSER="$new_user" createdb "$new_database"
 
 # Dump the origin database into a file
-echo "Dumping $origin_database into $DUMP_FILE"
-PGPASSWORD="$admin_password" PGHOST="$PGHOST" PGUSER="$admin_user" PGDATABASE="$origin_database" pg_dump -Z0 -Fc -f "$DUMP_FILE"
+echo "Dumping $origin_database into $dump_file"
+PGPASSWORD="$admin_password" PGHOST="$hostname" PGUSER="$admin_user" PGDATABASE="$origin_database" pg_dump -Z0 -Fc -f "$dump_file"
 
 # Restore the dump into the new database
-echo "Restoring $DUMP_FILE into $new_database"
-PGPASSWORD="$admin_password" PGHOST="$PGHOST" PGUSER="$admin_user" PGDATABASE="$new_database" pg_restore -d "$new_database" "$DUMP_FILE"
+echo "Restoring $dump_file into $new_database"
+PGPASSWORD="$admin_password" PGHOST="$hostname" PGUSER="$admin_user" PGDATABASE="$new_database" pg_restore -d "$new_database" "$dump_file"
 
 # Grant privileges to the user
 echo "Granting privileges to $new_user on $new_database"
-PGPASSWORD="$admin_password" PGHOST="$PGHOST" PGUSER="$admin_user" PGDATABASE="$new_database" psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $new_user;";
-PGPASSWORD="$admin_password" PGHOST="$PGHOST" PGUSER="$admin_user" PGDATABASE="$new_database" psql -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $new_user;";
+PGPASSWORD="$admin_password" PGHOST="$hostname" PGUSER="$admin_user" PGDATABASE="$new_database" psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $new_user;";
+PGPASSWORD="$admin_password" PGHOST="$hostname" PGUSER="$admin_user" PGDATABASE="$new_database" psql -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $new_user;";
