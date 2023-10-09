ef05a4495d8c3e6e84742020754703128acf8654
Sam Onaisi <samonaisi@gmail.com>
2023-10-09 16:15:12 +0200

    create google models

--- services/google/admin.py
+++ services/google/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 
-from .models import GoogleSyncErrors, LPIGoogleAccount, LPIGoogleGroup
+from .models import GoogleSyncErrors
 from .tasks import (
     create_google_group_task,
     create_google_user_task,
@@ -12,84 +12,6 @@ from .tasks import (
     update_google_group_task,
     update_google_user_task,
 )
-
-
-class LPIGoogleAccountAdmin(admin.ModelAdmin):
-    list_display = ("keycloak_id", "email", "given_name", "family_name")
-    search_fields = ("keycloak_id", "people_id", "email", "given_name", "family_name")
-    exclude = (
-        "last_login",
-        "permissions",
-        "groups",
-        "people_id",
-        "language",
-        "birthdate",
-        "pronouns",
-        "personal_description",
-        "short_description",
-        "professional_description",
-        "location",
-        "job",
-        "profile_picture",
-        "sdgs",
-        "facebook",
-        "mobile_phone",
-        "linkedin",
-        "medium",
-        "website",
-        "skype",
-        "landline_phone",
-        "twitter",
-        "people_data",
-        "type",
-    )
-    actions = []
-    list_filter = tuple()
-
-    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
-        return super().get_queryset(request).filter(groups__organizations__code="CRI")
-
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if "delete_selected" in actions:
-            del actions["delete_selected"]
-        return actions
-
-    class Meta:
-        verbose_name = "Google account"
-
-
-class LPIGoogleGroupAdmin(admin.ModelAdmin):
-    list_display = ("name", "email")
-    search_fields = ("name", "email")
-    exclude = (
-        "people_id",
-        "type",
-        "organization",
-        "header_image",
-        "logo_image",
-        "featured_projects",
-        "groups",
-        "publication_status",
-        "people_data",
-        "order",
-        "parent",
-        "sdgs",
-        "description",
-    )
-    actions = []
-
-    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
-        return super().get_queryset(request).filter(organization__code="CRI")
-
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if "delete_selected" in actions:
-            del actions["delete_selected"]
-        return actions
-
-    class Meta:
-        verbose_name = "Google group"
 
 
 class GoogleSyncErrorsAdmin(admin.ModelAdmin):
@@ -126,6 +48,4 @@ class GoogleSyncErrorsAdmin(admin.ModelAdmin):
         verbose_name = "Google sync error"
 
 
-admin.site.register(LPIGoogleAccount, LPIGoogleAccountAdmin)
-admin.site.register(LPIGoogleGroup, LPIGoogleGroupAdmin)
 admin.site.register(GoogleSyncErrors, GoogleSyncErrorsAdmin)
