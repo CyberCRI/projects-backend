6fc72bb3c497b795f6d256a16fb54be6c24da1cc
Sam Onaisi <samonaisi@gmail.com>
2024-01-05 18:44:56 +0100

    correct db setup migration

--- apps/commons/migrations/0001_initial.py
+++ apps/commons/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.operations import TrigramExtension
+from django.contrib.postgres.operations import TrigramExtension, UnaccentExtension
 from django.db import migrations
 
 
@@ -7,6 +7,14 @@ class Migration(migrations.Migration):
     dependencies = []
 
     operations = [
+        UnaccentExtension(),
+        migrations.RunSQL(
+            sql='CREATE TEXT SEARCH CONFIGURATION english_conf( COPY = english );'
+            'ALTER TEXT SEARCH CONFIGURATION english_conf '
+            'ALTER MAPPING FOR hword, hword_part, word '
+            'WITH unaccent, english_stem;',
+            reverse_sql='DROP TEXT SEARCH CONFIGURATION english_conf;'
+        ),
         migrations.RunSQL(
             'ALTER TEXT SEARCH CONFIGURATION english_conf '
             'ALTER MAPPING FOR numword, numhword, hword_numpart '
