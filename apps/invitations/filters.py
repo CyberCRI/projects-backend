ed6d0db298b09fe7c1b779ce796edb21d4dfbe9d
LPI User <atetu>
2024-01-09 14:12:59 +0100

    deleted filter file for access request

--- apps/invitations/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-
-from apps.invitations.models import AccessRequest
-
-
-class AccessRequestFilter(filters.FilterSet):
-    class Meta:
-        model = AccessRequest
-        fields = ["status"]
