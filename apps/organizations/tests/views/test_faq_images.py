7daabbc89ab752801d8f993b68b0edaaf5ae645d
Sam Onaisi <samonaisi@gmail.com>
2024-02-02 15:50:36 +0100

    test faq images

--- apps/organizations/tests/views/test_faq_images.py
+++ apps/organizations/tests/views/test_faq_images.py
@@ -1,64 +1,151 @@
-from apps.accounts.utils import get_superadmins_group
-from apps.commons.test import ImageStorageTestCaseMixin, JwtAPITestCase
-from apps.organizations.factories import OrganizationFactory
+from django.urls import reverse
+from faker import Faker
+from parameterized import parameterized
+from rest_framework import status
+
+from apps.accounts.factories import UserFactory
+from apps.commons.test import JwtAPITestCase, TestRoles
+from apps.files.models import Image
+from apps.organizations.factories import FaqFactory
+
+faker = Faker()
 
 
-class FaqImagesTestCase(JwtAPITestCase, ImageStorageTestCaseMixin):
-    list_view = "Faq-images-list"
-    detail_view = "Faq-images-detail"
-    field_name = "images"
-    base_permissions = []
-    org_permissions = ["organizations.change_faq"]
+class RetrieveFaqImageTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.faq = FaqFactory()
+        cls.image = cls.get_test_image()
+        cls.faq.images.add(cls.image)
 
-    # Tests for POST calls that should pass
-    def test_upload_images_base_permission(self):
-        user = self.create_user(self.base_permissions)
-        user.groups.add(get_superadmins_group())
-        organization = OrganizationFactory()
-        kwargs = {"organization_code": organization.code}
-        self.assert_image_upload(self.list_view, **kwargs)
-
-    def test_upload_images_org_permission(self):
-        _, _, organization = self.create_org_user(self.org_permissions)
-        kwargs = {"organization_code": organization.code}
-        self.assert_image_upload(self.list_view, **kwargs)
-
-    # Tests for POST calls that should fail
-    def test_upload_oversized_image(self):
-        user = self.create_user(self.base_permissions)
-        user.groups.add(get_superadmins_group())
-        organization = OrganizationFactory()
-        kwargs = {"organization_code": organization.code}
-        self.assert_image_too_large(self.list_view, **kwargs)
-
-    def test_upload_images_no_permission(self):
-        self.create_user()
-        organization = OrganizationFactory()
-        kwargs = {"organization_code": organization.code}
-        self.assert_image_upload(self.list_view, denied=True, **kwargs)
-
-    # Tests for DELETE calls that should pass
-    def test_delete_mtm_images_base_permission(self):
-        user = self.create_user(self.base_permissions)
-        user.groups.add(get_superadmins_group())
-        organization = OrganizationFactory()
-        kwargs = {"organization_code": organization.code}
-        self.assert_delete_mtm_image(
-            self.detail_view, self.field_name, organization.faq, denied=False, **kwargs
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS,),
+            (TestRoles.DEFAULT,),
+        ]
+    )
+    def test_retrieve_faq_image(self, role):
+        user = self.get_parameterized_test_user(role, instances=[])
+        self.client.force_authenticate(user)
+        response = self.client.get(
+            reverse(
+                "Faq-images-detail", args=(self.faq.organization.code, self.image.id)
+            )
         )
+        assert response.status_code == status.HTTP_302_FOUND
 
-    def test_delete_mtm_images_org_permission(self):
-        _, _, organization = self.create_org_user(self.org_permissions)
-        kwargs = {"organization_code": organization.code}
-        self.assert_delete_mtm_image(
-            self.detail_view, self.field_name, organization.faq, denied=False, **kwargs
-        )
 
-    # Tests for DELETE calls that should fail
-    def test_delete_mtm_images_no_permission(self):
-        self.create_user()
-        organization = OrganizationFactory()
-        kwargs = {"organization_code": organization.code}
-        self.assert_delete_mtm_image(
-            self.detail_view, self.field_name, organization.faq, denied=True, **kwargs
+class CreateFaqImageTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.faq = FaqFactory()
+
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_201_CREATED),
+            (TestRoles.ORG_ADMIN, status.HTTP_201_CREATED),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_create_faq_image(self, role, expected_code):
+        user = self.get_parameterized_test_user(role, instances=[self.faq.organization])
+        self.client.force_authenticate(user)
+        payload = {"file": self.get_test_image_file()}
+        response = self.client.post(
+            reverse("Faq-images-list", args=(self.faq.organization.code,)),
+            data=payload,
+            format="multipart",
         )
+        assert response.status_code == expected_code
+        if expected_code == status.HTTP_201_CREATED:
+            assert response.json()["static_url"] is not None
+
+
+class UpdateFaqTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.faq = FaqFactory()
+        cls.owner = UserFactory()
+
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.OWNER, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_200_OK),
+            (TestRoles.ORG_ADMIN, status.HTTP_200_OK),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_update_faq_image(self, role, expected_code):
+        image = self.get_test_image(owner=self.owner)
+        self.faq.images.add(image)
+        user = self.get_parameterized_test_user(
+            role, instances=[self.faq.organization], owned_instance=image
+        )
+        self.client.force_authenticate(user)
+        payload = {
+            "scale_x": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "scale_y": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "left": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "top": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "natural_ratio": faker.pyfloat(min_value=1.0, max_value=2.0),
+        }
+        response = self.client.patch(
+            reverse(
+                "Faq-images-detail",
+                args=(self.faq.organization.code, image.id),
+            ),
+            data=payload,
+            format="multipart",
+        )
+        assert response.status_code == expected_code
+        if expected_code == status.HTTP_200_OK:
+            assert response.json()["scale_x"] == payload["scale_x"]
+            assert response.json()["scale_y"] == payload["scale_y"]
+            assert response.json()["left"] == payload["left"]
+            assert response.json()["top"] == payload["top"]
+            assert response.json()["natural_ratio"] == payload["natural_ratio"]
+
+
+class DeleteFaqImageTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.faq = FaqFactory()
+        cls.owner = UserFactory()
+
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.OWNER, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_204_NO_CONTENT),
+            (TestRoles.ORG_ADMIN, status.HTTP_204_NO_CONTENT),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_delete_faq_image(self, role, expected_code):
+        image = self.get_test_image(owner=self.owner)
+        self.faq.images.add(image)
+        user = self.get_parameterized_test_user(
+            role, instances=[self.faq.organization], owned_instance=image
+        )
+        self.client.force_authenticate(user)
+        response = self.client.delete(
+            reverse(
+                "Faq-images-detail",
+                args=(self.faq.organization.code, image.id),
+            ),
+        )
+        assert response.status_code == expected_code
+        if expected_code == status.HTTP_204_NO_CONTENT:
+            assert not Image.objects.filter(id=image.id).exists()
