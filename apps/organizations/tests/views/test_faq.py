98a0a76028409bcf00fdadd0c291553f2440b4cb
Sam Onaisi <samonaisi@gmail.com>
2024-02-02 17:14:32 +0100

    test faq

--- apps/organizations/tests/views/test_faq.py
+++ apps/organizations/tests/views/test_faq.py
@@ -1,273 +1,133 @@
 from django.urls import reverse
+from faker import Faker
+from parameterized import parameterized
 from rest_framework import status
 
-from apps.accounts.factories import UserFactory
-from apps.accounts.utils import get_superadmins_group
-from apps.commons.test import JwtAPITestCase
-from apps.organizations import factories, models
+from apps.commons.test import JwtAPITestCase, TestRoles
+from apps.organizations.factories import FaqFactory, OrganizationFactory
+from apps.organizations.models import Faq
+
+faker = Faker()
 
 
-class FaqTestCaseNoPermission(JwtAPITestCase):
-    def test_create_no_permission(self):
-        fake = factories.FaqFactory.build()
-        organization = factories.OrganizationFactory()
-        self.client.force_authenticate(UserFactory())
-        payload = {
-            "title": fake.title,
-            "content": fake.content,
-            "organization_code": organization.code,
-        }
-        response = self.client.post(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+class CreateFaqTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.organization = OrganizationFactory()
 
-    def test_retrieve_no_permission(self):
-        organization = factories.OrganizationFactory()
-        organization_two = factories.OrganizationFactory()
-        self.client.force_authenticate(UserFactory())
-        response = self.client.get(
-            reverse("Faq-list", kwargs={"organization_code": organization.code})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
-        content = response.json()
-        self.assertNotEqual(organization_two.faq.title, content["title"])
-        self.assertNotEqual(organization_two.faq.content, content["content"])
-        self.assertEqual(organization.faq.title, content["title"])
-        self.assertEqual(organization.faq.content, content["content"])
-
-    def test_update_no_permission(self):
-        fake = factories.FaqFactory.build()
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": fake.title,
-            "content": fake.content,
-            "organization_code": organization.code,
-        }
-        self.client.force_authenticate(UserFactory())
-        response = self.client.put(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_partial_update_no_permission(self):
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": "New title",
-        }
-        self.client.force_authenticate(UserFactory())
-        response = self.client.patch(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_destroy_no_permission(self):
-        organization = factories.OrganizationFactory()
-        self.client.force_authenticate(UserFactory())
-        response = self.client.delete(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-
-class FaqTestCaseBasePermission(JwtAPITestCase):
-    def test_create_base_permission(self):
-        fake = factories.FaqFactory.build()
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": fake.title,
-            "content": fake.content,
-            "organization_code": organization.code,
-        }
-        user = UserFactory(groups=[get_superadmins_group()])
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_201_CREATED),
+            (TestRoles.ORG_ADMIN, status.HTTP_201_CREATED),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_create_faq(self, role, expected_code):
+        user = self.get_parameterized_test_user(role, instances=[self.organization])
         self.client.force_authenticate(user)
+        payload = {
+            "title": faker.sentence(nb_words=4),
+            "content": faker.text(),
+            "organization_code": self.organization.code,
+        }
         response = self.client.post(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
+            reverse("Faq-list", args=(self.organization.code,)),
             data=payload,
         )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        assert response.status_code == expected_code
+        if response.status_code == status.HTTP_201_CREATED:
+            content = response.json()
+            assert content["title"] == payload["title"]
+            assert content["content"] == payload["content"]
 
-        content = response.json()
-        self.assertIn("id", content)
 
-        faq = models.Faq.objects.get(id=content["id"])
-        self.assertEqual(fake.title, faq.title)
-        self.assertEqual(fake.content, faq.content)
-        self.assertEqual(organization.id, faq.organization.id)
-        payload["content"] = self.get_base64_image()
-        response = self.client.post(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.json())
-        self.assertEqual(len(response.json()["images"]), 1)
+class RetrieveFaqTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.organization = OrganizationFactory()
+        cls.faq = FaqFactory(organization=cls.organization)
 
-    def test_retrieve_base_permission(self):
-        organization = factories.OrganizationFactory()
-        organization_two = factories.OrganizationFactory()
-        user = UserFactory()
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS,),
+            (TestRoles.DEFAULT,),
+        ]
+    )
+    def test_retrieve_faq(self, role):
+        user = self.get_parameterized_test_user(role, instances=[self.organization])
         self.client.force_authenticate(user)
         response = self.client.get(
-            reverse("Faq-list", kwargs={"organization_code": organization.code})
+            reverse("Faq-list", args=(self.organization.code,)),
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
+        assert response.status_code == status.HTTP_200_OK
         content = response.json()
-        self.assertNotEqual(organization_two.faq.title, content["title"])
-        self.assertNotEqual(organization_two.faq.content, content["content"])
-        self.assertEqual(organization.faq.title, content["title"])
-        self.assertEqual(organization.faq.content, content["content"])
+        assert content["title"] == self.organization.faq.title
+        assert content["content"] == self.organization.faq.content
 
-    def test_update_base_permission(self):
-        fake = factories.FaqFactory.build()
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": "New title",
-            "content": fake.content,
-            "organization_code": organization.code,
-        }
-        user = UserFactory(groups=[get_superadmins_group()])
+
+class UpdateFaqTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.organization = OrganizationFactory()
+        cls.faq = FaqFactory(organization=cls.organization)
+
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_200_OK),
+            (TestRoles.ORG_ADMIN, status.HTTP_200_OK),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_update_faq(self, role, expected_code):
+        user = self.get_parameterized_test_user(role, instances=[self.organization])
         self.client.force_authenticate(user)
-        response = self.client.put(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        organization.faq.refresh_from_db()
-        self.assertEqual(organization.faq.title, "New title")
-        payload["content"] = self.get_base64_image()
-        response = self.client.put(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.json())
-        self.assertEqual(len(response.json()["images"]), 1)
-
-    def test_partial_update_base_permission(self):
-        organization = factories.OrganizationFactory()
         payload = {
-            "title": "New title",
+            "title": faker.sentence(nb_words=4),
+            "content": faker.text(),
         }
-        user = UserFactory(groups=[get_superadmins_group()])
-        self.client.force_authenticate(user)
         response = self.client.patch(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
+            reverse("Faq-list", args=(self.organization.code,)),
             data=payload,
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        organization.faq.refresh_from_db()
-        self.assertEqual(organization.faq.title, "New title")
-        payload["content"] = self.get_base64_image()
-        response = self.client.patch(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.json())
-        self.assertEqual(len(response.json()["images"]), 1)
+        assert response.status_code == expected_code
+        if response.status_code == status.HTTP_200_OK:
+            content = response.json()
+            assert content["title"] == payload["title"]
+            assert content["content"] == payload["content"]
 
-    def test_destroy_base_permission(self):
-        organization = factories.OrganizationFactory()
-        user = UserFactory(groups=[get_superadmins_group()])
+
+class DeleteFaqTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.organization = OrganizationFactory()
+
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_204_NO_CONTENT),
+            (TestRoles.ORG_ADMIN, status.HTTP_204_NO_CONTENT),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_delete_faq(self, role, expected_code):
+        faq = FaqFactory(organization=self.organization)
+        user = self.get_parameterized_test_user(role, instances=[self.organization])
         self.client.force_authenticate(user)
         response = self.client.delete(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
+            reverse("Faq-list", args=(self.organization.code,)),
         )
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(models.Faq.objects.filter(id=organization.faq.id).exists())
-
-
-class FaqTestCaseOrganizationPermission(JwtAPITestCase):
-    def test_create_org_permission(self):
-        fake = factories.FaqFactory.build()
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": fake.title,
-            "content": fake.content,
-            "organization_code": organization.code,
-        }
-        user = UserFactory(permissions=[("organizations.add_faq", organization)])
-        self.client.force_authenticate(user)
-        response = self.client.post(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        content = response.json()
-        self.assertIn("id", content)
-        faq = models.Faq.objects.get(id=content["id"])
-        self.assertEqual(fake.title, faq.title)
-        self.assertEqual(fake.content, faq.content)
-        self.assertEqual(organization.id, faq.organization.id)
-
-        payload["content"] = self.get_base64_image()
-        response = self.client.post(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.json())
-        self.assertEqual(len(response.json()["images"]), 1)
-
-    def test_retrieve_org_permission(self):
-        organization = factories.OrganizationFactory()
-        organization_two = factories.OrganizationFactory()
-        user = UserFactory()
-        self.client.force_authenticate(user)
-        response = self.client.get(
-            reverse("Faq-list", kwargs={"organization_code": organization.code})
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.content)
-        content = response.json()
-        self.assertNotEqual(organization_two.faq.title, content["title"])
-        self.assertNotEqual(organization_two.faq.content, content["content"])
-        self.assertEqual(organization.faq.title, content["title"])
-        self.assertEqual(organization.faq.content, content["content"])
-
-    def test_update_org_permission(self):
-        fake = factories.FaqFactory.build()
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": "New title",
-            "content": fake.content,
-            "organization_code": organization.code,
-        }
-        user = UserFactory(permissions=[("organizations.change_faq", organization)])
-        self.client.force_authenticate(user)
-        response = self.client.put(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        organization.faq.refresh_from_db()
-        self.assertEqual(organization.faq.title, "New title")
-        payload["content"] = self.get_base64_image()
-        response = self.client.put(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.json())
-        self.assertEqual(len(response.json()["images"]), 1)
-
-    def test_partial_update_org_permission(self):
-        organization = factories.OrganizationFactory()
-        payload = {
-            "title": "New title",
-        }
-        user = UserFactory(permissions=[("organizations.change_faq", organization)])
-        self.client.force_authenticate(user)
-        response = self.client.patch(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        organization.faq.refresh_from_db()
-        self.assertEqual(organization.faq.title, "New title")
-
-        payload["content"] = self.get_base64_image()
-        response = self.client.patch(
-            reverse("Faq-list", kwargs={"organization_code": organization.code}),
-            data=payload,
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK, response.json())
-        self.assertEqual(len(response.json()["images"]), 1)
+        assert response.status_code == expected_code
+        if response.status_code == status.HTTP_204_NO_CONTENT:
+            assert not Faq.objects.filter(id=faq.id).exists()
