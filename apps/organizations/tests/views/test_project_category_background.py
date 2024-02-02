f049db81852bd6cfd4dea24beabae75bed7173d0
Sam Onaisi <samonaisi@gmail.com>
2024-02-02 16:54:29 +0100

    category background tests

--- apps/organizations/tests/views/test_project_category_background.py
+++ apps/organizations/tests/views/test_project_category_background.py
@@ -1,74 +1,130 @@
-from apps.accounts.utils import get_superadmins_group
-from apps.commons.test import ImageStorageTestCaseMixin, JwtAPITestCase
+from django.urls import reverse
+from faker import Faker
+from parameterized import parameterized
+from rest_framework import status
+
+from apps.accounts.factories import UserFactory
+from apps.commons.test import JwtAPITestCase, TestRoles
+from apps.files.models import Image
 from apps.organizations.factories import OrganizationFactory, ProjectCategoryFactory
 
+faker = Faker()
 
-class ProjectCategoryBackgroundTestCase(JwtAPITestCase, ImageStorageTestCaseMixin):
-    list_view = "Category-background-list"
-    detail_view = "Category-background-detail"
-    field_name = "background_image"
-    base_permissions = []
-    org_permissions = ["organizations.change_projectcategory"]
 
-    @staticmethod
-    def create_project_category(organization):
-        project_category = ProjectCategoryFactory()
-        project_category.organization = organization
-        project_category.save()
-        return project_category
+class CreateProjectCategoryBackgroundTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.organization = OrganizationFactory()
+        cls.category = ProjectCategoryFactory(organization=cls.organization)
 
-    # Tests for POST calls that should pass
-    def test_upload_images_base_permission(self):
-        user = self.create_user(self.base_permissions)
-        user.groups.add(get_superadmins_group())
-        project_category = ProjectCategoryFactory()
-        kwargs = {"category_id": project_category.id}
-        self.assert_image_upload(self.list_view, **kwargs)
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.SUPERADMIN, status.HTTP_201_CREATED),
+            (TestRoles.ORG_ADMIN, status.HTTP_201_CREATED),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_create_project_category_background(self, role, expected_code):
+        user = self.get_parameterized_test_user(role, instances=[self.organization])
+        self.client.force_authenticate(user)
+        payload = {"file": self.get_test_image_file()}
+        response = self.client.post(
+            reverse("Category-background-list", args=(self.category.id,)),
+            data=payload,
+            format="multipart",
+        )
+        assert response.status_code == expected_code
 
-    # Tests for POST calls that should fail
-    def test_upload_oversized_image(self):
-        user = self.create_user(self.base_permissions)
-        user.groups.add(get_superadmins_group())
-        project_category = ProjectCategoryFactory()
-        kwargs = {"category_id": project_category.id}
-        self.assert_image_too_large(self.list_view, **kwargs)
 
-    def test_upload_images_org_permission(self):
-        _, _, organization = self.create_org_user(self.org_permissions)
-        project_category = self.create_project_category(organization)
-        kwargs = {"category_id": project_category.id}
-        self.assert_image_upload(self.list_view, **kwargs)
-
-    def test_upload_images_no_permission(self):
-        self.create_user()
-        project_category = ProjectCategoryFactory()
-        kwargs = {"category_id": project_category.id}
-        self.assert_image_upload(self.list_view, denied=True, **kwargs)
-
-    # Tests for DELETE calls that should pass
-    def test_delete_fk_images_base_permission(self):
-        user = self.create_user(self.base_permissions)
-        user.groups.add(get_superadmins_group())
-        project_category = ProjectCategoryFactory()
-        kwargs = {"category_id": project_category.id}
-        self.assert_delete_fk_image(
-            self.detail_view, self.field_name, project_category, denied=False, **kwargs
+class UpdateProjectCategoryBackgroundTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.owner = UserFactory()
+        cls.image = cls.get_test_image(owner=cls.owner)
+        cls.organization = OrganizationFactory()
+        cls.category = ProjectCategoryFactory(
+            organization=cls.organization,
+            background_image=cls.image,
         )
 
-    def test_delete_fk_images_org_permission(self):
-        _, _, organization = self.create_org_user(self.org_permissions)
-        project_category = self.create_project_category(organization)
-        kwargs = {"category_id": project_category.id}
-        self.assert_delete_fk_image(
-            self.detail_view, self.field_name, project_category, denied=False, **kwargs
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.OWNER, status.HTTP_200_OK),
+            (TestRoles.SUPERADMIN, status.HTTP_200_OK),
+            (TestRoles.ORG_ADMIN, status.HTTP_200_OK),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_update_project_category_background(self, role, expected_code):
+        user = self.get_parameterized_test_user(
+            role, instances=[self.organization], owned_instance=self.image
         )
+        self.client.force_authenticate(user)
+        payload = {
+            "scale_x": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "scale_y": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "left": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "top": faker.pyfloat(min_value=1.0, max_value=2.0),
+            "natural_ratio": faker.pyfloat(min_value=1.0, max_value=2.0),
+        }
+        response = self.client.patch(
+            reverse(
+                "Category-background-detail",
+                args=(self.category.id, self.image.id),
+            ),
+            data=payload,
+            format="multipart",
+        )
+        assert response.status_code == expected_code
+        if expected_code == status.HTTP_200_OK:
+            assert response.json()["scale_x"] == payload["scale_x"]
+            assert response.json()["scale_y"] == payload["scale_y"]
+            assert response.json()["left"] == payload["left"]
+            assert response.json()["top"] == payload["top"]
+            assert response.json()["natural_ratio"] == payload["natural_ratio"]
 
-    # Tests for DELETE calls that should fail
-    def test_delete_fk_images_user(self):
-        organization = OrganizationFactory()
-        project_category = self.create_project_category(organization)
-        self.create_user()
-        kwargs = {"category_id": project_category.id}
-        self.assert_delete_fk_image(
-            self.detail_view, self.field_name, project_category, denied=True, **kwargs
+
+class DeleteProjectCategoryBackgroundTestCase(JwtAPITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.organization = OrganizationFactory()
+        cls.owner = UserFactory()
+
+    @parameterized.expand(
+        [
+            (TestRoles.ANONYMOUS, status.HTTP_401_UNAUTHORIZED),
+            (TestRoles.DEFAULT, status.HTTP_403_FORBIDDEN),
+            (TestRoles.OWNER, status.HTTP_204_NO_CONTENT),
+            (TestRoles.SUPERADMIN, status.HTTP_204_NO_CONTENT),
+            (TestRoles.ORG_ADMIN, status.HTTP_204_NO_CONTENT),
+            (TestRoles.ORG_FACILITATOR, status.HTTP_403_FORBIDDEN),
+            (TestRoles.ORG_USER, status.HTTP_403_FORBIDDEN),
+        ]
+    )
+    def test_delete_project_category_background(self, role, expected_code):
+        image = self.get_test_image(owner=self.owner)
+        category = ProjectCategoryFactory(
+            organization=self.organization, background_image=image
         )
+        user = self.get_parameterized_test_user(
+            role, instances=[self.organization], owned_instance=image
+        )
+        self.client.force_authenticate(user)
+        response = self.client.delete(
+            reverse(
+                "Category-background-detail",
+                args=(category.id, image.id),
+            ),
+        )
+        assert response.status_code == expected_code
+        if expected_code == status.HTTP_204_NO_CONTENT:
+            assert not Image.objects.filter(id=image.id).exists()
