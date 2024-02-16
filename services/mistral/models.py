ebd2027f66cafd46faa81bbfd96247a7d69d9113
Sam Onaisi <samonaisi@gmail.com>
2024-02-16 17:13:49 +0100

    return created instance in embedding model vectorize method

--- services/mistral/models.py
+++ services/mistral/models.py
@@ -92,11 +92,12 @@ class Embedding(models.Model):
         self.queued_for_update = False
         self.prompt_hashcode = self.hash_prompt(prompt)
         self.save()
+        return self
 
     @transaction.atomic
     def vectorize(self, summary: Optional[str] = None) -> "Embedding":
         if self.set_visibility() or self.embed_if_not_visible:
-            self._vectorize(summary)
+            return self._vectorize(summary)
         return self
 
     @classmethod
@@ -117,8 +118,7 @@ class Embedding(models.Model):
     ) -> "Embedding":
         instance, created = cls.objects.get_or_create(item=item)
         if skip_queue:
-            instance.vectorize()
-            return instance
+            return instance.vectorize()
         if created or (
             instance.prompt_hashcode != instance.hash_prompt()
             and not instance.queued_for_update
