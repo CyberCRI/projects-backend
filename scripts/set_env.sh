a7697d63fe0d689df70989c7cf706a9e44de161e
Sam Onaisi <samonaisi@gmail.com>
2024-04-16 17:15:10 +0200

    fix: correct db_name in set_env script

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -26,7 +26,7 @@ case ${1} in
   alice )
     export PGHOST=dev-lab-projects-backend.postgres.database.azure.com;
     export PGPORT=5432;
-    export PGDATABASE=alice;
+    export PGDATABASE=dev_alice;
     export PGUSER=dev_tech;
     export PGPASSWORD=${PGPASSWORD:?"Specify PGPASSWORD to connect to db."};
     export AZURE_ACCOUNT_NAME=criparisdevlabprojects;
