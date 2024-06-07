d413f198eed0db71a96ce77f3dc85f13dba2c309
Stanislas Bruhière <stanislas@bruhiere.fr>
2024-06-07 10:28:42 +0200

    feat: improve backup / restore scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,8 @@
 # Dump database in a pgdump file.
 
 # Set env : choices are local, dev, rbac, stag and prod
+set -euo pipefail
+
 FROM=${1:?"missing arg 1 for origin environment [local/dev/rbac/stag/prod]"}
 source ./scripts/set_env.sh ${FROM}
 
@@ -11,6 +13,6 @@ BACKUP_PATH="dumps/${BACKUP_FILE}.pgdump"
 
 echo "Backing up ${PGDATABASE} to ${BACKUP_FILE}"
 
-pg_dump -Z6 -Fc -f ${BACKUP_PATH}
+pg_dump -Z6 -Fc -f ${BACKUP_PATH} --verbose
 
 echo "Backup completed"
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,6 +2,8 @@
 # Restore pgdump to a database
 
 # Set env : choices are local, dev, rbac, stag and prod
+set -euo pipefail
+
 TO=${1:?"missing arg 1 for destination environment [local/dev/rbac/stag/prod]"}
 BACKUP_FILE=${2:?"missing arg 2 for backup file name"}
 BACKUP_PATH="dumps/${BACKUP_FILE}.pgdump"
@@ -11,6 +13,6 @@ echo "Restoring ${BACKUP_FILE} to ${PGDATABASE}"
 
 dropdb --if-exists --force ${PGDATABASE}
 createdb -T template0 ${PGDATABASE}
-pg_restore --no-owner --no-acl -d ${PGDATABASE} ${BACKUP_PATH}
+pg_restore --no-owner --no-acl -d ${PGDATABASE} ${BACKUP_PATH} --verbose
 
 echo "${PGDATABASE} database restored"
