4c00a5dc306b59e4a33b15dc43f269c2fc3402eb
samonaisi <samonaisi@gmail.com>
2024-06-12 19:36:24 +0200

    PROJ-1685-Cr-er-les-pages-de-liaison-d-un-compte-Keycloak-un-comptes-IDP-externe (#219)
    * feat: add external idp to keycloak realm export
    
    * fix fullstack environment settings
    
    * update fullstack storage url
    
    * lint and format

--- projects/settings/fullstack.py
+++ projects/settings/fullstack.py
@@ -1,56 +1,12 @@
-import requests
-from celery.schedules import crontab
-
-from projects.settings.base import REQUESTS_DEFAULT_TIMEOUT
-from projects.settings.develop import *  # noqa: F401, F403
+from projects.settings.local import *  # noqa: F401, F403
 
 ENVIRONMENT = "fullstack"
 
 FRONTEND_URL = "https://localhost:8080"
 PUBLIC_URL = "http://localhost:8000"
 
-SPECTACULAR_SETTINGS["SWAGGER_UI_SETTINGS"]["url"] = "/api/schema/"  # noqa: F405
-
-##############
-#   CELERY   #
-##############
-
-CELERY_BEAT_SCHEDULE = {
-    "send_notifications_reminder": {
-        "task": "apps.notifications.tasks.send_notifications_reminder",
-        "schedule": crontab(minute="*", hour="*"),
-    }
-}
-
-##############
-#   GOOGLE   #
-##############
-
-GOOGLE_EMAIL_PREFIX = "fullstack"
-
-##############
-#  KEYCLOAK  #
-##############
-
-KEYCLOAK_SERVER_URL = "http://keycloak:8080/"
-KEYCLOAK_PUBLIC_KEY = requests.get(
-    f"{KEYCLOAK_SERVER_URL}realms/lp", timeout=REQUESTS_DEFAULT_TIMEOUT
-).json()["public_key"]
-KEYCLOAK_PUBLIC_KEY = (
-    f"-----BEGIN PUBLIC KEY-----\n{KEYCLOAK_PUBLIC_KEY}\n-----END PUBLIC KEY-----"
+AWS_S3_ENDPOINT_URL = (
+    "http://minio:9000"  # change to "http://localhost:9000" to read from local Minio
 )
 
-##############
-# SIMPLE_JWT #
-##############
-
-# From https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("Bearer", "JWT", "Invite", "Service"),
-    "USER_ID_FIELD": "keycloak_account__keycloak_id",
-    "USER_ID_CLAIM": "sub",
-    "TOKEN_TYPE_CLAIM": "typ",
-    "AUTH_TOKEN_CLASSES": ("apps.accounts.authentication.BearerToken",),
-    "ALGORITHM": "RS256",
-    "VERIFYING_KEY": KEYCLOAK_PUBLIC_KEY,
-}
+GOOGLE_EMAIL_PREFIX = "fullstack"
