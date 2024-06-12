7b3e7998dcb94e7401b0a43f6b65ce2abbb4b7c5
Sam Onaisi <samonaisi@gmail.com>
2024-06-12 16:16:22 +0200

    update fullstack storage url

--- projects/settings/fullstack.py
+++ projects/settings/fullstack.py
@@ -5,6 +5,6 @@ ENVIRONMENT = "fullstack"
 FRONTEND_URL = "https://localhost:8080"
 PUBLIC_URL = "http://localhost:8000"
 
-AWS_S3_ENDPOINT_URL = "http://localhost:9000"
+AWS_S3_ENDPOINT_URL = "http://minio:9000"  # change to "http://localhost:9000" to read from local Minio
 
 GOOGLE_EMAIL_PREFIX = "fullstack"
