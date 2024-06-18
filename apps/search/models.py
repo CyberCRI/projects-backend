6122f022b2ba3cf51d83d4c7c5f5d0f757cd123f
Sam Onaisi <samonaisi@gmail.com>
2024-06-18 17:28:14 +0200

    feat: add unique model for mixed algolia indexing

---
+++ apps/search/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+class SearchObject(models.Model):
+    """
+    SearchObject instance to store objects that can be searched.
+    Several types of objects can be stored in this model.
+
+    Attributes
+    ----------
+    ----------
+    project: ForeignKey
+        Project instance.
+    people_group: ForeignKey
+        PeopleGroup instance.
+    user: ForeignKey
+        ProjectUser instance.
+    type: CharField
+        Type of the object.
+    """
+
+    class SearchObjectType(models.TextChoices):
+        """"""
+
+        PROJECT = "project"
+        PEOPLE_GROUP = "people_group"
+        USER = "user"
+
+    project = models.ForeignKey(
+        "projects.Project",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="search_object",
+    )
+    people_group = models.ForeignKey(
+        "accounts.PeopleGroup",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="search_object",
+    )
+    user = models.ForeignKey(
+        "accounts.ProjectUser",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="search_object",
+    )
+    type = models.CharField(max_length=50, choices=SearchObjectType.choices)
+
+    @property
+    def item(self):
+        match self.type:
+            case self.SearchObjectType.PROJECT:
+                return self.project
+            case self.SearchObjectType.PEOPLE_GROUP:
+                return self.people_group
+            case self.SearchObjectType.USER:
+                return self.user
+    
