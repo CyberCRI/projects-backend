e4b8729cc32b513298a3c3ee56064943c81e3777
Sam Onaisi <samonaisi@gmail.com>
2024-06-21 16:15:11 +0200

    fix: make search filters inactive on irrelevant types

--- apps/search/filters.py
+++ apps/search/filters.py
@@ -12,65 +12,83 @@ class SearchObjectFilter(filters.FilterSet):
     types = MultiValueCharFilter(field_name="type", lookup_expr="in")
     organizations = MultiValueCharFilter(method="filter_organizations")
     sdgs = MultiValueCharFilter(method="filter_sdgs")
-
     # User filters
     skills = MultiValueCharFilter(method="filter_skills")
-
     # Project filters
-    languages = MultiValueCharFilter(field_name="project__language", lookup_expr="in")
-    categories = MultiValueCharFilter(
-        field_name="project__categories__id", lookup_expr="in"
-    )
-    members = UserMultipleIDFilter(
-        field_name="project__groups__users__id", lookup_expr="in", distinct=True
-    )
-    wikipedia_tags = MultiValueCharFilter(
-        field_name="project__wikipedia_tags__wikipedia_qid", lookup_expr="in"
-    )
-    organization_tags = MultiValueCharFilter(
-        field_name="project__organization_tags__id", lookup_expr="in"
-    )
+    languages = MultiValueCharFilter(method="filter_languages")
+    categories = MultiValueCharFilter(method="filter_categories")
+    members = UserMultipleIDFilter(method="filter_members")
+    wikipedia_tags = MultiValueCharFilter(method="filter_wikipedia_tags")
+    organization_tags = MultiValueCharFilter(method="filter_organization_tags")
 
     def filter_organizations(self, queryset, name, value):
         return queryset.filter(
-            (
-                Q(type=SearchObject.SearchObjectType.PEOPLE_GROUP)
-                & Q(people_group__organization__code__in=value)
-            )
-            | (
-                Q(type=SearchObject.SearchObjectType.PROJECT)
-                & Q(project__organizations__code__in=get_hierarchy_codes(value))
-            )
-            | (
-                Q(type=SearchObject.SearchObjectType.USER)
-                & Q(user__groups__organizations__code__in=value)
-            )
+            Q(project__organizations__code__in=get_hierarchy_codes(value))
+            | Q(people_group__organization__code__in=value)
+            | Q(user__groups__organizations__code__in=value)
         ).distinct()
 
     def filter_sdgs(self, queryset, name, value):
         return queryset.filter(
-            (
-                Q(type=SearchObject.SearchObjectType.PEOPLE_GROUP)
-                & Q(people_group__sdgs__overlap=value)
-            )
-            | (
-                Q(type=SearchObject.SearchObjectType.PROJECT)
-                & Q(project__sdgs__overlap=value)
-            )
-            | (
-                Q(type=SearchObject.SearchObjectType.USER)
-                & Q(user__sdgs__overlap=value)
-            )
+            Q(project__sdgs__overlap=value)
+            | Q(people_group__sdgs__overlap=value)
+            | Q(user__sdgs__overlap=value)
         ).distinct()
 
     def filter_skills(self, queryset, name, value):
+        unaffected_types = [
+            SearchObject.SearchObjectType.PROJECT,
+            SearchObject.SearchObjectType.PEOPLE_GROUP,
+        ]
         return queryset.filter(
-            (
-                Q(type=SearchObject.SearchObjectType.USER)
-                & Q(people_group__skills__wikipedia_tag__wikipedia_qid__in=value)
-            )
-            | Q(type=SearchObject.SearchObjectType.PROJECT)
-            | Q(type=SearchObject.SearchObjectType.PEOPLE_GROUP)
+            Q(people_group__skills__wikipedia_tag__wikipedia_qid__in=value)
+            | Q(type__in=unaffected_types)
+        ).distinct()
+
+    def filter_languages(self, queryset, name, value):
+        unaffected_types = [
+            SearchObject.SearchObjectType.USER,
+            SearchObject.SearchObjectType.PEOPLE_GROUP,
+        ]
+        return queryset.filter(
+            Q(project__language__in=value) | Q(type__in=unaffected_types)
+        ).distinct()
+
+    def filter_categories(self, queryset, name, value):
+        unaffected_types = [
+            SearchObject.SearchObjectType.USER,
+            SearchObject.SearchObjectType.PEOPLE_GROUP,
+        ]
+        return queryset.filter(
+            Q(project__categories__id__in=value) | Q(type__in=unaffected_types)
+        ).distinct()
+
+    def filter_members(self, queryset, name, value):
+        unaffected_types = [
+            SearchObject.SearchObjectType.USER,
+            SearchObject.SearchObjectType.PEOPLE_GROUP,
+        ]
+        return queryset.filter(
+            Q(project__groups__users__id__in=value) | Q(type__in=unaffected_types)
+        ).distinct()
+
+    def filter_wikipedia_tags(self, queryset, name, value):
+        unaffected_types = [
+            SearchObject.SearchObjectType.USER,
+            SearchObject.SearchObjectType.PEOPLE_GROUP,
+        ]
+        return queryset.filter(
+            Q(project__wikipedia_tags__wikipedia_qid__in=value)
+            | Q(type__in=unaffected_types)
+        ).distinct()
+
+    def filter_organization_tags(self, queryset, name, value):
+        unaffected_types = [
+            SearchObject.SearchObjectType.USER,
+            SearchObject.SearchObjectType.PEOPLE_GROUP,
+        ]
+        return queryset.filter(
+            Q(project__organization_tags__id__in=value) | Q(type__in=unaffected_types)
         ).distinct()
 
     class Meta:
