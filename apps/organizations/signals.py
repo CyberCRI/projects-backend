95ec04b38b62fb926a33cd460cd2918eef226e8e
Sam Onaisi <samonaisi@gmail.com>
2024-06-24 15:04:13 +0200

    feat: create signals to avoid having org with no faq

--- apps/organizations/signals.py
+++ apps/organizations/signals.py
@@ -1,8 +1,25 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+
+from .models import Faq
 
 
 @receiver(pre_delete, sender="organizations.Organization")
 def delete_organization_roles(sender, instance, **kwargs):
     """Delete the associated roles."""
     instance.groups.all().delete()
+
+
+@receiver(post_save, sender="organizations.Organization")
+def create_organization_faq(sender, instance, created, **kwargs):
+    """Create a FAQ for the organization."""
+    if instance.faq is None:
+        instance.faq = Faq.objects.create(title="", content="")
+        instance.save()
+
+
+@receiver(pre_delete, sender="organizations.Faq")
+def create_new_organization_faq(sender, instance, **kwargs):
+    """Create a new FAQ for the organization."""
+    instance.organization.faq = Faq.objects.create(title="", content="")
+    instance.organization.save()
