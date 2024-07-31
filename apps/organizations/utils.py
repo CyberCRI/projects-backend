5e248755beb1e1d16ef1db4d70ac5a553249e754
Sam Onaisi <samonaisi@gmail.com>
2024-07-31 16:48:47 +0200

    fix: get_above_hierarchy_codes

--- apps/organizations/utils.py
+++ apps/organizations/utils.py
@@ -3,7 +3,7 @@ from typing import List
 from apps.organizations.models import Organization
 
 
-def get_below_hierarchy_codes(codes: List[str]) -> List:
+def get_below_hierarchy_codes(codes: List[str]) -> List[str]:
     """
     Get all the codes of the organizations below in the hierarchy of the given codes
     """
@@ -12,18 +12,23 @@ def get_below_hierarchy_codes(codes: List[str]) -> List:
         hierarchy_codes += codes
         codes = (
             Organization.objects.values("code")
-            .filter(parent__code__in=codes, is_logo_visible_on_parent_dashboard=True)
+            .filter(parent__code__in=codes)
             .values_list("code", flat=True)
         )
     return hierarchy_codes
 
 
-def get_above_hierarchy_codes(organization: Organization) -> List[Organization]:
+def get_above_hierarchy_codes(codes: List[str]) -> List[str]:
     """
     Get all the codes of the organizations above in the hierarchy of the given codes
     """
-    parents_codes = [organization]
-    while organization.parent and organization.is_logo_visible_on_parent_dashboard:
-        organization = organization.parent
-        parents_codes.append(organization)
-    return parents_codes
+    hierarchy_codes = []
+    while len(codes) > 0:
+        hierarchy_codes += codes
+        visible_on_parent_dashboard = Organization.objects.filter(
+            code__in=codes, is_logo_visible_on_parent_dashboard=True
+        )
+        codes = Organization.objects.filter(
+            children__in=visible_on_parent_dashboard
+        ).values_list("code", flat=True)
+    return hierarchy_codes
