65355886f68d6f9ad5d97bd3b8d7c2bd02a11314
samonaisi <samonaisi@gmail.com>
2024-08-09 10:32:25 +0200

    PROJ-1764-Garder-les-erreurs-de-vectorisation-dans-la-db (#263)

--- services/mistral/tasks.py
+++ services/mistral/tasks.py
@@ -18,14 +18,8 @@ def _vectorize_updated_objects():
     projects = Project.objects.all()
     users = ProjectUser.objects.all()
     for project in projects:
-        try:
-            embedding, _ = ProjectEmbedding.objects.get_or_create(item=project)
-            embedding.vectorize()
-        except Exception as e:  # noqa: PIE786
-            logger.error(f"Error while vectorizing project {project.pk}: {e}")
+        embedding, _ = ProjectEmbedding.objects.get_or_create(item=project)
+        embedding.vectorize()
     for user in users:
-        try:
-            embedding, _ = UserEmbedding.objects.get_or_create(item=user)
-            embedding.vectorize()
-        except Exception as e:  # noqa: PIE786
-            logger.error(f"Error while vectorizing user {user.pk}: {e}")
+        embedding, _ = UserEmbedding.objects.get_or_create(item=user)
+        embedding.vectorize()
