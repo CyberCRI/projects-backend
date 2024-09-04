b87f68ae3133877fe5b025b0fe0861a16653cf94
Stanislas Bruhière <stanislas@bruhiere.fr>
2024-09-04 14:25:41 +0200

    feat: rework stats

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -1,6 +1,13 @@
 from rest_framework_nested import routers
+from apps.commons.urls import organization_router_register
 
 from .views import StatsViewSet
 
 router = routers.SimpleRouter()
-router.register(r"stats", StatsViewSet, basename="Stats")
+
+organization_router_register(
+    router,
+    r"stats",
+    StatsViewSet,
+    basename="Stats",
+)
