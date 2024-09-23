9b3770a28f449f17bb358a2df57aaaf674c90976
Sam Onaisi <samonaisi@gmail.com>
2024-09-23 17:36:40 +0200

    feat: add json format mistral chat completion

--- services/mistral/interface.py
+++ services/mistral/interface.py
@@ -1,4 +1,5 @@
-from typing import List
+import json
+from typing import Any, Dict, List
 
 from django.conf import settings
 from mistralai import Mistral
@@ -12,12 +13,30 @@ class MistralService:
         """
         Get the chat response from Mistral API.
         """
-        system = [{"content": message, "role": "system"} for message in system]
-        prompt = [{"content": message, "role": "user"} for message in prompt]
+        messages = [
+            *[{"content": message, "role": "system"} for message in system],
+            *[{"content": message, "role": "user"} for message in prompt],
+        ]
         response = cls.service.chat.complete(
-            model="mistral-small", messages=system + prompt, **kwargs
+            model="mistral-small", messages=messages, **kwargs
         )
         return "\n".join([choice.message.content for choice in response.choices])
+
+    @classmethod
+    def get_json_chat_response(
+        cls, system: List[str], prompt: List[str], **kwargs
+    ) -> Dict[str, Any]:
+        messages = [
+            *[{"content": message, "role": "system"} for message in system],
+            *[{"content": message, "role": "user"} for message in prompt],
+        ]
+        response = cls.service.chat.complete(
+            model="mistral-small",
+            messages=messages,
+            response_format={"type": "json_object"},
+            **kwargs
+        )
+        return json.loads(response.choices[0].message.content)
 
     @classmethod
     def get_embedding(cls, prompt: str) -> List[float]:
