cf5ca7cb710eb61c01d20eb8ba7631e7301eca9c
samonaisi <samonaisi@gmail.com>
2024-09-23 10:16:57 +0200

    PROJ-1838-Erreur-sur-les-taches-celery-de-mise-jour-des-index (#289)
    * fix: only execute index update signals if instance is correct
    
    * feat: improve index signal to handle project soft delete

--- apps/search/signals.py
+++ apps/search/signals.py
@@ -1,10 +1,11 @@
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
-from apps.accounts.models import ProjectUser
+from apps.accounts.models import PeopleGroup, ProjectUser
 from apps.projects.models import Project
 
 from .tasks import (
+    delete_project_search_object_task,
     update_or_create_people_group_search_object_task,
     update_or_create_project_search_object_task,
     update_or_create_user_search_object_task,
@@ -16,13 +17,15 @@ from .tasks import (
 @receiver(post_save, sender="accounts.ProjectUser")
 def update_search_object_on_user_save(sender, instance, created, **kwargs):
     """Create the associated search object at user's creation."""
-    update_or_create_user_search_object_task.delay(instance.pk)
+    if isinstance(instance, ProjectUser):
+        update_or_create_user_search_object_task.delay(instance.pk)
 
 
 @receiver(post_save, sender="accounts.Skill")
 def update_search_object_on_user_skill_save(sender, instance, created, **kwargs):
     """Create the associated search object at user's creation."""
-    update_or_create_user_search_object_task.delay(instance.user.pk)
+    if isinstance(instance.user, ProjectUser):
+        update_or_create_user_search_object_task.delay(instance.user.pk)
 
 
 @receiver(post_save, sender="accounts.PrivacySettings")
@@ -30,7 +33,8 @@ def update_search_object_on_user_privacy_settings_save(
     sender, instance, created, **kwargs
 ):
     """Create the associated search object at user's creation."""
-    update_or_create_user_search_object_task.delay(instance.user.pk)
+    if isinstance(instance.user, ProjectUser):
+        update_or_create_user_search_object_task.delay(instance.user.pk)
 
 
 @receiver(m2m_changed, sender=ProjectUser.groups.through)
@@ -46,7 +50,11 @@ def update_search_object_on_user_role_change(sender, instance, action, **kwargs)
 @receiver(post_save, sender="projects.Project")
 def update_search_object_on_project_save(sender, instance, created, **kwargs):
     """Create the associated search object at project's creation."""
-    update_or_create_project_search_object_task.delay(instance.pk)
+    if isinstance(instance, Project):
+        if instance.deleted_at is not None:
+            delete_project_search_object_task.delay(instance.pk)
+        else:
+            update_or_create_project_search_object_task.delay(instance.pk)
 
 
 @receiver(m2m_changed, sender=Project.organizations.through)
@@ -61,7 +69,7 @@ def update_search_object_on_project_organization_change(
 @receiver(m2m_changed, sender=Project.categories.through)
 def update_search_object_on_project_category_change(sender, instance, action, **kwargs):
     """Create the associated search object at project's creation."""
-    if action in ["post_add", "post_remove"]:
+    if isinstance(instance, Project) and action in ["post_add", "post_remove"]:
         update_or_create_project_search_object_task.delay(instance.pk)
 
 
@@ -70,7 +78,7 @@ def update_search_object_on_project_wikipedia_tags_change(
     sender, instance, action, **kwargs
 ):
     """Create the associated search object at project's creation."""
-    if action in ["post_add", "post_remove"]:
+    if isinstance(instance, Project) and action in ["post_add", "post_remove"]:
         update_or_create_project_search_object_task.delay(instance.pk)
 
 
@@ -79,7 +87,7 @@ def update_search_object_on_project_organization_tags_change(
     sender, instance, action, **kwargs
 ):
     """Create the associated search object at project's creation."""
-    if action in ["post_add", "post_remove"]:
+    if isinstance(instance, Project) and action in ["post_add", "post_remove"]:
         update_or_create_project_search_object_task.delay(instance.pk)
 
 
@@ -89,4 +97,5 @@ def update_search_object_on_project_organization_tags_change(
 @receiver(post_save, sender="accounts.PeopleGroup")
 def update_or_create_people_group_search_object(sender, instance, created, **kwargs):
     """Create the associated search object at people group's creation."""
-    update_or_create_people_group_search_object_task.delay(instance.pk)
+    if isinstance(instance, PeopleGroup):
+        update_or_create_people_group_search_object_task.delay(instance.pk)
