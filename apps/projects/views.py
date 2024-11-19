d0eab1055cb03820c9eea1e32cd13c3da05e4cc8
samonaisi <samonaisi@gmail.com>
2024-11-19 14:03:54 +0100

    fix: remove atomicity for project and group creation to allow indexing (#314)
    * remove atomicity for project and group creation to allow indexing
    
    * fix broken google test

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -133,7 +133,6 @@ class ProjectViewSet(MultipleIDViewsetMixin, viewsets.ModelViewSet):
         """Adds request to the serializer's context."""
         return {"request": self.request}
 
-    @transaction.atomic
     def perform_create(self, serializer: ProjectSerializer):
         project = serializer.save()
         project.setup_permissions(self.request.user)
