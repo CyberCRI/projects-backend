4e011eb3dc04feed6ed65f4bb5d15a91dd36909f
samonaisi <samonaisi@gmail.com>
2024-11-20 17:35:46 +0100

    feat: easily activate toolbar in dev (#317)
    * feat: add toolbar in any case
    
    * feat: add ingress nginx ip
    
    * fix: toolbar config fix
    
    * feat: debug False in dev by default
    
    * format and lint
    
    ---------
    
    Co-authored-by: Stanislas Bruhiere <stanislas@bruhiere.fr>

--- projects/settings/base.py
+++ projects/settings/base.py
@@ -65,6 +65,9 @@ SILENCED_SYSTEM_CHECKS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
 
+
+DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda _request: DEBUG}
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -529,6 +532,7 @@ if DEBUG:
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
         "127.0.0.1",
         "10.0.2.2",
+        "10.244.2.4",
     ]
 
 # Python requests module default timeout
