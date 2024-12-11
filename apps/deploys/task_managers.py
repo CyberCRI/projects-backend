a1153a39cff8b9b7726fe7bc4620ee769c27f036
samonaisi <samonaisi@gmail.com>
2024-12-11 13:42:27 +0100

    PROJ-1970-Toujours-lancer-la-tache-de-migration-au-d-marrage (#336)

--- apps/deploys/task_managers.py
+++ apps/deploys/task_managers.py
@@ -12,33 +12,66 @@ from .tasks import (
 
 
 class PostDeployTask:
+    """
+    Base class for post deploy tasks
+
+    All subclasses of this class will create a PostDeployProcess object
+    in the database with the attributes defined in the subclass.
+
+    These processes are executed by the PostDeployProcess.deploy() method.
+
+    Attributes:
+    task_name: str
+        Name of the task, must be unique
+    priority: int
+        Priority of the task, lower numbers will be executed first
+    task: Callable[[int], None]
+        Task to be executed
+    run_in_tests: bool = False
+        If the task should be executed in the tests environment
+    run_in_celery: bool = True
+        If the task should be executed in celery
+    sleep_time: int = 30
+        Time (in minutes) during which the task should not be executed again
+        if the deploy() method is called multiple times in a short period.
+    """
+
     task_name: str
     priority: int
     task: Callable[[int], None]
     run_in_tests: bool = False
     run_in_celery: bool = True
+    sleep_time: int = 30  # in minutes
 
     @classmethod
     def run(cls):
+        """Run the task"""
         return cls.task()
 
     @classmethod
     def celery_run(cls):
+        """Run the task in celery"""
         return cls.task.delay()
 
     def get_progress(self):
-        return None
+        """Get the current progress of the task"""
+        return None  # noqa: R501
 
 
 class Migrate(PostDeployTask):
+    """Apply all migrations"""
+
     task_name = "migrate"
     priority = 1
     task = migrate
     run_in_tests = True
     run_in_celery = False
+    sleep_time = 0
 
 
 class BaseGroupsPermissions(PostDeployTask):
+    """Assign permissions to the default and superadmins groups"""
+
     task_name = "base_groups_permissions"
     priority = 2
     task = base_groups_permissions
@@ -46,12 +79,22 @@ class BaseGroupsPermissions(PostDeployTask):
 
 
 class AlgoliaReindex(PostDeployTask):
+    """Reindex all searchable models in Algolia"""
+
     task_name = "algolia_reindex"
     priority = 3
     task = algolia_reindex_task
 
 
 class InstanceGroupsPermissions(PostDeployTask):
+    """
+    Assign permissions to the default groups of all models that inherit from
+    PermissionsSetupModel :
+    - Project
+    - PeopleGroup
+    - Organization
+    """
+
     task_name = "instance_groups_permissions"
     priority = 4
     task = instance_groups_permissions
@@ -66,6 +109,8 @@ class InstanceGroupsPermissions(PostDeployTask):
 
 
 class RemoveDuplicatedRoles(PostDeployTask):
+    """If a user has redundant roles, remove the less powerful ones"""
+
     task_name = "remove_duplicated_roles"
     priority = 5
     task = remove_duplicated_roles
