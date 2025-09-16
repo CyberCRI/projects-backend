7c1ee9fafe949a66bc6a326d1eeb28c4dd722249
Sam Onaisi <samonaisi@gmail.com>
2025-09-16 17:49:49 +0200

    create tac on org creation

--- apps/organizations/signals.py
+++ apps/organizations/signals.py
@@ -1,8 +1,17 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+
+from .models import TermsAndConditions
 
 
 @receiver(pre_delete, sender="organizations.Organization")
 def delete_organization_roles(sender, instance, **kwargs):
     """Delete the associated roles."""
     instance.groups.all().delete()
+
+
+@receiver(post_save, sender="organizations.Organization")
+def create_terms_and_conditions(sender, instance, created, **kwargs):
+    """Create the associated terms and conditions at user's creation."""
+    if created:
+        TermsAndConditions.objects.get_or_create(organization=instance)
