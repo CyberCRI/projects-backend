5e37822ded9ec8012f235b728643a33b42436e55
rgermain <contact@germainremi.fr>
2025-11-05 13:00:25 +0100

    feat(command): add command to generate json for frontend translate

--- services/crisalid/management/commands/generate_crisalid_locales.py
+++ services/crisalid/management/commands/generate_crisalid_locales.py
@@ -1,19 +1,30 @@
 import json
+import re
 
 from django.core.management.base import BaseCommand
 
 from services.crisalid.models import Document
 from services.crisalid.relators import choices
 
+INVALID_CHAR_REGEX = re.compile(r"[^a-zA-Z0-9-_]")
+
 
 class Command(BaseCommand):
     help = "create json files from crisalid for frontend locales"  # noqa: A003
 
+    def sanitize_key(self, key: str) -> str:
+        """some key are same in it, so replace with -"""
+
+        return INVALID_CHAR_REGEX.sub("-", key).strip("-")
+
     def handle(self, **options):
         data = {
-            "relators": {val: val for val in sorted(v for _, v in choices)},
+            "relators": {
+                self.sanitize_key(val): val for val in sorted(v for _, v in choices)
+            },
             "document_types": {
-                val: val for val in sorted(doc.value for doc in Document.DocumentType)
+                self.sanitize_key(val): val
+                for val in sorted(doc.value for doc in Document.DocumentType)
             },
         }
 
