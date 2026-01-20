cdba77a72a7d9265e23ddd87d447f2dfe54ad926
JulienDroulez <julien.droulez@free.fr>
2026-01-20 16:33:07 +0100

    fix: missing expected data for e2e tests (#501)
    * fix: missing expected data for e2e tests
    
    * fix: lint
    
    * enable e2e hardcoded tag in search
    
    ---------
    
    Co-authored-by: Julien Droulez <julien.droulez@learningplanetinstitute.org>
    Co-authored-by: Sam Onaisi <samonaisi@gmail.com>

--- apps/commons/management/commands/seed_db.py
+++ apps/commons/management/commands/seed_db.py
@@ -269,3 +269,20 @@ class Command(BaseCommand):
                 project=project, author=random.choice([owner] + members)  # nosec B311
             )
         self.stdout.write(self.style.SUCCESS("Project related items created."))
+
+        # expected by e2e frontend tests
+        e2e_tag = TagFactory(
+            organization=organization,
+            title="Biology",
+            title_en="Biology",
+            title_fr="Biologie",
+            description="Biology",
+            description_en="Biology",
+            description_fr="Biologie",
+        )
+        e2e_classification = TagClassificationFactory(
+            organization=organization, tags=[e2e_tag]
+        )
+        organization.enabled_projects_tag_classifications.add(e2e_classification)
+        organization.enabled_skills_tag_classifications.add(e2e_classification)
+        self.stdout.write(self.style.SUCCESS("E2E frontend test tag created."))
