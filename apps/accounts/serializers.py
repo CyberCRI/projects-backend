d80689a4345c7ed9e054661fa66b5f7d903fc7ca
rgermain <contact@germainremi.fr>
2026-02-12 12:23:07 +0100

    linter

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Any, Dict, List, Optional, Union
+from typing import Any
 
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
