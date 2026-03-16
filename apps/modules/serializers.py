adc2ae843c77b849fc69c678a7c70dafc487109c
rgermain <contact@germainremi.fr>
2026-03-16 18:27:04 +0100

    optimize: modules serializers

--- apps/modules/serializers.py
+++ apps/modules/serializers.py
@@ -1,3 +1,4 @@
+from django.http import QueryDict
 from rest_framework import serializers
 
 
@@ -6,16 +7,25 @@ class ModulesSerializers(serializers.ModelSerializer):
 
     modules = serializers.SerializerMethodField()
 
+    def __init__(self, *ar, **kw):
+        super().__init__(*ar, **kw)
+
+        request = self.context.get("request")
+        query = request.query_params if request else QueryDict()
+
+        if "modules_keys" not in self.context:
+            modules_keys = None
+            # if modules is set queryparams , return list elements (for multiples modules)
+            if "modules" in query:
+                modules_keys = query.getlist("modules")
+            # if modules is not set, get "default" values from Meta serializer
+            if modules_keys is None:
+                modules_keys = getattr(self.Meta, "modules_keys", None)
+            self.context["modules_keys"] = tuple(modules_keys) if modules_keys else None
+
     def get_modules(self, instance):
         request = self.context.get("request")
-
-        modules_keys = None
-        # if modules is set queryparams , return list elements (for multiples modules)
-        if "modules" in request.query_params:
-            modules_keys = request.query_params.getlist("modules")
-        # if modules is not set, get "default" values from Meta serializer
-        if modules_keys is None:
-            modules_keys = getattr(self.Meta, "modules_keys", None)
+        modules_keys = self.context.get("modules_keys")
 
         modules_manager = instance.get_related_module()
         return modules_manager(instance, user=request.user).count(modules_keys)
