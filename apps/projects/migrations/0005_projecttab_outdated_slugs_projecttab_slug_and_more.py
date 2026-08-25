83f13357537ac569b1674c225e8d64f2b534dbe9
Rémi Germain <contact@germainremi.fr>
2026-08-25 11:24:03 +0200

    fix: migrations (#652)
    * fix: migrations
    
    * fix: migrations
    
    * fix: migrations

--- apps/projects/migrations/0005_projecttab_outdated_slugs_projecttab_slug_and_more.py
+++ apps/projects/migrations/0005_projecttab_outdated_slugs_projecttab_slug_and_more.py
@@ -11,7 +11,7 @@ def generate_tab_slug(apps, schema_editor):
         slug = tab.generate_slug()
         tab.slug = slug
         index = 0
-        while ProjectTab.objects.slug_or_id(tab.slug).exists():
+        while ProjectTab.objects.using(conn_alias).slug_or_id(tab.slug).exists():
             index += 1
             tab.slug = f"{slug}-{index}"
         tab.save(update_fields=["slug"], using=conn_alias)
@@ -37,7 +37,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="projecttab",
             name="slug",
-            field=models.SlugField(default=None, null=True),
+            # don't create index in db to avoid error in migrations
+            field=models.SlugField(default=None, db_index=False, unique=False),
             preserve_default=False,
         ),
         migrations.RunPython(generate_tab_slug, atomic=True),
